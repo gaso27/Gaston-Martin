9303e970e85fa4a7555152586eef1f2226d356b2
gaso27 <gaso27@hotmail.com>
2023-11-03 22:31:52 -0300

    Primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form')
+    contactName = document.getElementById('contact-name')
+    contactEmail = document.getElementById('contact-email')
+    contactProject = document.getElementById('contact-project')
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // Checkear si los campos tienen valor
+    if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+        // agregar y quitar color
+    contactMessage.classList.remove('color-blue')
+    contactMessage.classList.add('color-red')
+
+    // Mostrar Mensaje
+    contactMessage.textContent = 'Completa los campos vacios 📩'
+    }else{
+        // ServiceID - TemplateID - #form - publicKey
+        emailjs.sendForm('service_wgbaamb' , 'template_9bg2cel' , '#contact-form' , 'tgVnCoGUopMfzzalb')
+            .then(() =>{
+                // Mostrar Mensaje y agregar color
+                contactMessage.classList.add('color-blue')
+                contactMessage.textContent = 'Mensaje enviado ✅'
+
+                // Eliminar mensaje despues de 5 segundos
+                setTimeout(() =>{
+                    contactMessage.textContent = ''
+                }, 5000)
+            }, (error) =>{
+                alert('OOPS! ALGO SALIO MAL...!', error)
+            })
+        // Limpiar Campos
+        contactName.value = ''
+        contactEmail.value = ''
+        contactProject.value = ''
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
